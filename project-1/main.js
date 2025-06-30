5de24b0ef410238f2882ba338ddc13b699683416
Ashbel <kashbel747@gmail.com>
2025-06-30 19:19:27 +0300

    Complete section: Completed project 4

--- project-1/main.js
+++ project-1/main.js
@@ -136,3 +136,45 @@ resetBtn.addEventListener('click', function(){
     document.getElementById('timer').innerHTML = "00:00:00";
 })
 
+//Variables
+const addTask = document.getElementById('add-task');
+const taskContainer = document.getElementById('task-box');
+const inputTask = document.getElementById('input-task');
+
+//Event Listener for Add Button
+addTask.addEventListener('click', function(){
+    let task = document.createElement('div');
+    task.classList.add('task');
+
+    let li = document.createElement('li');
+    li.innerText = `${inputTask.value}`;
+    task.appendChild(li);
+
+    let checkButton = document.createElement("button");
+    checkButton.innerHTML = '<i class="fa-solid fa-check"></i>';
+    checkButton.classList.add('checkTask');
+    task.appendChild(checkButton);
+
+    let deleteButton = document.createElement("button");
+    deleteButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+    deleteButton.classList.add('deleteTask');
+    task.appendChild(deleteButton);
+
+    if(inputTask.value === ""){
+        alert('Please enter a task');
+    } else{
+        taskContainer.appendChild(task);
+    }
+
+    inputTask.value = "";
+
+    checkButton.addEventListener('click', function(){
+        checkButton.parentElement.style.textDecoration = "line-through";
+    });
+
+    deleteButton.addEventListener('click', function(e){
+        let target = e.target;
+
+        target.parentElement.remove();
+    })
+})
